28ffa74e14faaf8933399cba85eaba1cc34d6fd2
LuisM <luism6977@gmail.com>
2021-01-23 16:28:29 -0300

    Deploy project

--- src/index.js
+++ src/index.js
@@ -30,7 +30,7 @@ const store = createStore(
 
 const app = (
   <Provider store={store}>
-    <BrowserRouter>
+    <BrowserRouter basename="my-burger-app">
       <App />
     </BrowserRouter>
   </Provider>
